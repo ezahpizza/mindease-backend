7d05b74dcfaba9e47711ac94dbf22ec72730194c
Prateek Mohapatra <143695291+ezahpizza@users.noreply.github.com>
2025-05-26 17:58:34 +0530

    Configured CORS for frontend

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://mindease-eight.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
